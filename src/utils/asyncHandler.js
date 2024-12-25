be208aa9e7f04c79f88854db35271f4bc20c320d
lucky <luckydamamii2410@gmail.com>
2024-12-25 18:48:14 +0530

    util code discussion

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,20 @@
+//Type -1
+const asyncHandler = (requestHandler) => {
+  return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+//Type-2
+/*const asyncHandler = (func) => async (req, res, next) => {
+  try {
+    await func(req, res, next);
+  } catch (error) {
+    res.status(err.code || 500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+};*/
