f740fdab1406f0adcc187ef720f466da5b31b1f3
lucky <luckydamamii2410@gmail.com>
2025-01-03 19:39:26 +0530

    user and video models is completed

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,4 +1,7 @@
 import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 const videoSchema = new Schema(
   {
     videoFile: {
@@ -31,9 +34,11 @@ const videoSchema = new Schema(
     },
     owner: {
       type: Schema.Types.ObjectId,
+      ref: "User",
     },
   },
   { timestamps: true }
 );
 
+videoSchema.plugin(mongooseAggregatePaginate);
 export const Video = mongoose.model("Video", videoSchema);
